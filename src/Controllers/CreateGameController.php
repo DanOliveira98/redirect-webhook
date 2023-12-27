0235cf5eb0cec5b5254b3bb3c9d40b8b243acce8
danoliveira <daniel.oliveira.blv.13@gmail.com>
2023-12-27 20:56:39 -0300

    Fixed controller route

--- src/Controllers/CreateGameController.php
+++ src/Controllers/CreateGameController.php
@@ -2,14 +2,19 @@
 
 namespace GamesPackage\Controllers;
 
+use GamesPackage\Services\Pg;
+use GamesPackage\Services\Pragmatic;
 use GuzzleHttp\Psr7\Request;
 
 class CreateGameController
 {
-    public function createGame(Request $requestx)
+    public function createGame(Request $request)
     {
-        if ($requestx->provider === "PG") {
-
+        if ($request->provider === "pg") {
+            return app()->make(Pg::class)->play($request);
+        }
+        if ($request->provider === "pragmatic") {
+            return app()->make(Pragmatic::class)->play($request);
         }
     }
 }
